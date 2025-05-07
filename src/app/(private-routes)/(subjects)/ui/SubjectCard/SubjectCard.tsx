d55e3f0a65ae105ab3ad081c7a45a250a1ae917d
raudbrand <lysenkov.v.a@yandex.ru>
2025-05-07 15:40:08 +0300

    design improvements

--- src/app/(private-routes)/(subjects)/ui/SubjectCard/SubjectCard.tsx
+++ src/app/(private-routes)/(subjects)/ui/SubjectCard/SubjectCard.tsx
@@ -2,25 +2,28 @@
 
 import React, { memo } from "react";
 import { SubjectEntity } from "../../model/types/SubjectEntity";
-import { App, Card, Flex, Skeleton, Typography } from "antd";
+import { App, Card, Flex, Typography } from "antd";
 import { useRouter } from "next/navigation";
 import { deleteSubjectByIdThunk } from "../../model/thunks/deleteSubjectByIdThunk";
 import { useAppDispatch } from "@/app/lib/store";
 import { EditCardButton } from "@/app/UI/EditCardButton";
 import { DeleteCardButton } from "@/app/UI/DeleteCardButton";
 import { useSubjectFilters } from "@/app/(private-routes)/(subjects)/ui/SubjectsFilterPanel/hooks/useSubjectFilters";
-import { HighlightedText } from "@/app/UI/HighlightedText/HighlightedText";
 import { Picture } from "@/app/UI/Picture";
 import { LabelWithIcon } from "@/app/UI/LabelWithIcon";
 import {
     CARD_ICON_SIZE,
     FORM_ICON_SIZE,
 } from "@/app/UI/AppLayout/config/consts";
-import percentPng from "../../../../lib/assets/png/percent.png";
 import agePng from "../../../../lib/assets/png/age.png";
-import genderPng from "../../../../lib/assets/png/gender.png";
+import textPng from "../../../../lib/assets/png/textField.png";
+import subgroupPng from "../../../../lib/assets/png/subgroup.png";
+import calendarPng from "../../../../lib/assets/png/calendar.png";
+import userPng from "../../../../lib/assets/png/user.png";
 import { BOX_SHADOW, BOX_SHADOW_WIDTH } from "@/app/lib/themes/primary-theme";
 import deletePng from "@/app/lib/assets/png/delete.png";
+import dayjs from "dayjs";
+import { UserHelper } from "@/app/(private-routes)/(users)/model/helpers/UserHelper";
 
 export interface SubjectCardProps {
     subject?: SubjectEntity;
@@ -43,29 +46,29 @@ export const SubjectCard = memo((props: SubjectCardProps) => {
                 margin: BOX_SHADOW_WIDTH,
                 boxShadow: BOX_SHADOW,
             }}
-            title={
-                !isLoading ? (
-                    <Flex align={"center"} justify={"center"}>
-                        <HighlightedText
-                            style={{
-                                fontSize: 16,
-                            }}
-                            text={subject?.name ?? ""}
-                            search={highlightedSearch}
-                        />
-                    </Flex>
-                ) : (
-                    <Flex align={"center"} justify={"center"}>
-                        <Skeleton.Node
-                            style={{
-                                width: 50,
-                                height: 15,
-                            }}
-                            active
-                        />
-                    </Flex>
-                )
-            }
+            // title={
+            //     !isLoading ? (
+            //         <Flex align={"center"} justify={"center"}>
+            //             <HighlightedText
+            //                 style={{
+            //                     fontSize: 16,
+            //                 }}
+            //                 text={subject?.name ?? ""}
+            //                 search={highlightedSearch}
+            //             />
+            //         </Flex>
+            //     ) : (
+            //         <Flex align={"center"} justify={"center"}>
+            //             <Skeleton.Node
+            //                 style={{
+            //                     width: 50,
+            //                     height: 15,
+            //                 }}
+            //                 active
+            //             />
+            //         </Flex>
+            //     )
+            // }
             size={"small"}
             actions={[
                 <EditCardButton
@@ -125,25 +128,27 @@ export const SubjectCard = memo((props: SubjectCardProps) => {
                 align={"start"}
                 justify={"start"}
                 gap={16}
-                vertical
                 style={{ width: "100%" }}
             >
+                <Picture
+                    shape={"picture"}
+                    pictureWidth={150}
+                    pictureHeight={150}
+                    value={
+                        !subject?.photos || subject.photos.length === 0
+                            ? undefined
+                            : subject?.photos[0]
+                    }
+                />
                 <Flex
-                    align={"start"}
-                    justify={"start"}
-                    gap={16}
-                    style={{ width: "100%" }}
+                    style={{
+                        width: "100%",
+                    }}
+                    align={"center"}
+                    justify={"space-between"}
+                    gap={32}
+                    vertical
                 >
-                    <Picture
-                        shape={"picture"}
-                        pictureWidth={150}
-                        pictureHeight={150}
-                        value={
-                            !subject?.photos || subject.photos.length === 0
-                                ? undefined
-                                : subject?.photos[0]
-                        }
-                    />
                     <Flex
                         style={{
                             width: "100%",
@@ -153,52 +158,58 @@ export const SubjectCard = memo((props: SubjectCardProps) => {
                         gap={4}
                         vertical
                     >
-                        <HighlightedText
-                            style={{ fontSize: 20, fontWeight: "bold" }}
-                            text={subject?.antropologicalType?.name}
-                            search={highlightedSearch}
-                        />
-                        <HighlightedText
-                            style={{ fontSize: 18 }}
-                            text={subject?.subgroup?.name}
+                        <LabelWithIcon
+                            imageSrc={textPng.src}
+                            labelText={subject?.name ?? "-"}
+                            iconSize={CARD_ICON_SIZE}
                             search={highlightedSearch}
                         />
                         <LabelWithIcon
-                            imageSrc={genderPng.src}
-                            labelText={subject?.gender?.name[0] ?? ""}
+                            imageSrc={subgroupPng.src}
+                            labelText={subject?.subgroup?.name ?? "-"}
                             iconSize={CARD_ICON_SIZE}
-                            textStyle={{ color: "navy", fontSize: 16 }}
                             search={highlightedSearch}
                         />
                         <LabelWithIcon
                             imageSrc={agePng.src}
                             labelText={subject?.age ?? ""}
                             iconSize={CARD_ICON_SIZE}
-                            textStyle={{ color: "gray", fontSize: 16 }}
                             search={highlightedSearch}
                         />
-                        <Flex
-                            style={{ width: "100%" }}
-                            align={"center"}
-                            justify={"start"}
-                            gap={8}
-                        >
-                            <LabelWithIcon
-                                imageSrc={percentPng.src}
-                                labelText={subject?.portraitMatch ?? ""}
-                                iconSize={CARD_ICON_SIZE}
-                                textStyle={{ color: "indianred", fontSize: 16 }}
-                                search={highlightedSearch}
-                            />
-                        </Flex>
+                    </Flex>
+                    <Flex
+                        style={{
+                            width: "100%",
+                        }}
+                        align={"start"}
+                        justify={"start"}
+                        gap={4}
+                        vertical
+                    >
+                        <LabelWithIcon
+                            imageSrc={calendarPng.src}
+                            labelText={
+                                subject?.date
+                                    ? dayjs(subject.date).format("DD.MM.YYYY")
+                                    : "-"
+                            }
+                            iconSize={CARD_ICON_SIZE}
+                            search={highlightedSearch}
+                        />
+                        <LabelWithIcon
+                            imageSrc={userPng.src}
+                            labelText={
+                                subject?.user
+                                    ? UserHelper.getSurnameWithInitials(
+                                          subject.user,
+                                      )
+                                    : "-"
+                            }
+                            iconSize={CARD_ICON_SIZE}
+                            search={highlightedSearch}
+                        />
                     </Flex>
                 </Flex>
-                <HighlightedText
-                    style={{ fontSize: 12, color: "gray" }}
-                    text={subject?.notes ?? ""}
-                    rowsCount={2}
-                    search={highlightedSearch}
-                />
             </Flex>
         </Card>
     );
